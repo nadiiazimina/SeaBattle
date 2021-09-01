afdf276e4147fd6f429fc4ab4228a61b4519466e
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2021-09-01 23:05:03 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Battleship</title>
+    <style>
+      body {
+        background-color: black;
+      }
+      div#board {
+        position: relative;
+        width: 1024px;
+        height: 863px;
+        margin: auto;
+        background: url("board.jpg") no-repeat;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="board">
+      
+    </div>
+    <script src="battleship.js"></script>
+   </body>
+  </html>
