9cdbb7a8450f3e45dbbbc65c2e4007ad65c1d9aa
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2021-09-09 13:26:03 +0300

    done board

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ var view = {
   }
 };
 
-view.displayMiss("00");
+/*view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
 view.displayHit("12");
 view.displayMiss("25");
 view.displayHit("26");
 
-view.displayMessage("Tap tap, is this thing on?");
+view.displayMessage("Tap tap, is this thing on?");*/
