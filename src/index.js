0f119b64fa75d281f8445ad085ee746bf037c4af
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2021-09-10 11:05:41 +0300

    Implementing the model object

--- src/index.js
+++ src/index.js
@@ -15,11 +15,24 @@ var view = {
   }
 };
 
-/*view.displayMiss("00");
+view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
 view.displayHit("12");
 view.displayMiss("25");
 view.displayHit("26");
 
-view.displayMessage("Tap tap, is this thing on?");*/
+view.displayMessage("Tap tap, is this thing on?");
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipSunk: 0,
+
+  ships: [
+    { location: ["06", "16", "26"], hits: ["", "", ""] },
+    { location: ["24", "34", "44"], hits: ["", "", ""] },
+    { location: ["10", "11", "12"], hits: ["", "", ""] }
+  ]
+};
